e07abfdb6a964c29b7bf8e2862809ec77accd35f
celesteck <celestekeyes6@gmail.com>
2022-05-01 15:27:23 -0700

    subtract wrong

---
+++ SkillDemoTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import java.beans.Transient;
+
+import org.junit.*;
+
+public class SkillDemoTest {
+    
+    @Test 
+    public void subtract(){
+
+        assertEquals(3, SkillDemo.subtract(4, 2));
+    }
+
+}
